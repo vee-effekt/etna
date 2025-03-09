7c7b23c5365f2833e1044ca85a76a1370a1a69e2
Ubuntu <lapwing@seas.upenn.edu>
2025-03-09 17:54:31 +0000

    works

---
+++ workloads/OCaml/BST/lib/Strategies/baseStagedType.mli
@@ -0,0 +1,1 @@
+module BaseStagedType : Base_quickcheck.Test.S with type t = Impl.tree
--- workloads/OCaml/BST/lib/Strategies/nat.ml
+++ workloads/OCaml/BST/lib/Strategies/nat.ml
@@ -1,8 +1,6 @@
 module Nat = struct
-  include Core.Int
-
-  type t = Core.Int.t [@@deriving sexp, quickcheck]
+  include Core;;
+  type t = int [@@deriving sexp, quickcheck]
   let quickcheck_generator =
-    let open Base_quickcheck.Generator in
-    int >>= fun i -> return (i % 128)
+    Base_quickcheck.Generator.int_uniform_inclusive Int.min_value Int.max_value
 end
