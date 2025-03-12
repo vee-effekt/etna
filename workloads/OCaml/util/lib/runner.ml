705661459005a735f3cc968c77c10d9ff526d53d
Joseph Cutler <jwc@seas.upenn.edu>
2025-03-12 02:10:32 +0000

    blah

--- workloads/OCaml/util/lib/runner.ml
+++ workloads/OCaml/util/lib/runner.ml
@@ -7,10 +7,8 @@ type btest = unit -> unit
 (* rename of the Core module type *)
 type 'a basegen = (module Base_quickcheck.Test.S with type t = 'a)
 
-type 'a testres = Succeeded | FailedWith of 'a
-
 (* Generalizing pre and post conditions *)
-type 'a test = Pre of bool * ('a test) | Post of ('a testres)
+type test = Pre of bool * test | Post of bool
 
 let ( =>> ) pre post = Pre (pre, post)
 let ( ->> ) pre post = Pre (pre, Post post)
@@ -25,23 +23,21 @@ type 'a property = {
 }
 
 (* Functions for realizing preconditions *)
-let rec qmake (t : 'a test) : bool =
+let rec qmake (t : test) : bool =
   match t with
   | Pre (pre, post) ->
       QCheck.assume pre;
       qmake post
-  | Post Succeeded -> true
-  | Post (FailedWith _) -> false
+  | Post b -> b
 
-let rec cmake (t : 'a test) : unit =
+let rec cmake (t : test) : unit =
   match t with
   | Pre (pre, post) ->
       Crowbar.guard pre;
       cmake post
-  | Post Succeeded -> Crowbar.check true
-  | Post (FailedWith _) -> Crowbar.check false
+  | Post b -> Crowbar.check b
   
-let rec bmake (t : 'a test) : (unit,'a) result =
+let rec bmake (t : test) : unit Base.Or_error.t =
   match t with
   | Pre (true, post) ->
       (* Printf.printf "Processing Pre-condition: true\n"; *)
@@ -50,12 +46,12 @@ let rec bmake (t : 'a test) : (unit,'a) result =
       (* Printf.printf "Skipping test due to false pre-condition\n"; *)
       (* false precondition, we can skip test *)
       Ok ()
-  | Post Succeeded ->
+  | Post true ->
       (* Printf.printf "Post-condition passed: true\n"; *)
       Ok ()
-  | Post (FailedWith v) ->
+  | Post false  ->
       (* Printf.printf "Post-condition failed: false\n"; *)
-      Error v
+      Error (Base.Error.of_string "fail")
 
 (* Helpers to build `'a property` types. Note that `'b` is the input to the property, INCLUDING the other parameters. *)
 let qbuild (g : 'b QCheck.arbitrary) (f : 'b -> bool) : string -> qtest =
@@ -65,21 +61,25 @@ let qbuild (g : 'b QCheck.arbitrary) (f : 'b -> bool) : string -> qtest =
 let cbuild (g : ('c, unit) Crowbar.gens) (f : 'c) : string -> ctest =
  fun name () -> Crowbar.add_test ~name g f
 
-let _verbose (type a) (g : (module Base_quickcheck.Test.S with type t = a)) (res : (unit,a * 'e) result) =
+ let _verbose res =
   match res with
-  | Ok() -> print_endline "tests passed?"
-  | Error((v,_)) ->
-    let module G = (val g : Base_quickcheck.Test.S with type t = a) in
-    print_endline ("bug found, cex: " ^ (Sexplib0.Sexp.to_string_hum (G.sexp_of_t v)))
+  | Ok () -> print_endline "Tests passed?"
+  | Error (_,err) -> print_endline "bug found!";
+                     print_endline (Base.Error.to_string_hum err)
 
-let bbuild (g : 'b basegen) (f : 'b -> (unit,'b) result) ?(seed : string option = None) : string -> btest =
+let bbuild (g : 'b basegen) (f : 'b -> unit Base.Or_error.t) ?(seed : string option = None) : string -> btest =
   fun _ () ->
     let seed_config =
       match seed with
       | Some s when not (String.equal s "") -> Base_quickcheck.Test.Config.Seed.Deterministic s
       | _ -> Base_quickcheck.Test.Config.Seed.Nondeterministic
     in
-    Base_quickcheck.Test.result ~f g
+    Base_quickcheck.Test.result ~f:(fun x ->
+      let res = f x in
+      match res with
+      | Ok () -> Ok ()
+      | Error _ ->  Error (Base.Error.of_string (Sexplib.Sexp.to_string_hum x))
+      ) g
       ~config:
         {
           seed = seed_config;
@@ -87,5 +87,5 @@ let bbuild (g : 'b basegen) (f : 'b -> (unit,'b) result) ?(seed : string option 
           shrink_count = 0;
           sizes = Base_quickcheck.Test.default_config.sizes;
         }
-    |> (_verbose g)
+    |> _verbose
   
