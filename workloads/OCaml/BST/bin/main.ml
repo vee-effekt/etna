10e11ff82bedafd57ad2c19fd7377004415e58b3
Joseph Cutler <jwc@seas.upenn.edu>
2025-03-12 01:39:02 +0000

    bug found

--- workloads/OCaml/BST/bin/main.ml
+++ workloads/OCaml/BST/bin/main.ml
@@ -10,32 +10,24 @@ open Sexplib;;
 open Stdio
 open Ppx_staged;;
 open Nat;;
+open BST;;
 
-open BST
-(*
+(* type nat = Nat.t * Nat.t [@@deriving wh, sexp]
+
 let () =
+  let generator = G.jit ~extra_cmi_paths:["/home/ubuntu/waffle-house/ppx_staged/_build/default/bin/.main.eobjs/byte"] staged_quickcheck_generator_nat in
+  let () = G.print staged_quickcheck_generator_nat in  
   let random_a = Splittable_random.State.of_int 1 in
   let random_b = Splittable_random.State.of_int 1 in
-  let random_c = Splittable_random.State.of_int 1 in
-  let random_d = Splittable_random.State.of_int 1 in
   let size = 10 in
   for _ = 1 to 10 do
     printf "\n";
     printf "\n";
-    let values = Base_quickcheck.Generator.generate BaseType.quickcheck_generator ~size ~random:random_a in
-    let staged_values_sr = Base_quickcheck.Generator.generate BaseType_Staged_SR.quickcheck_generator ~size ~random:random_b in
-    let staged_values_c = Base_quickcheck.Generator.generate BaseType_Staged_C.quickcheck_generator ~size ~random:random_c in
-    let staged_values_csr = Base_quickcheck.Generator.generate BaseType_Staged_CSR.quickcheck_generator ~size ~random:random_d in
-    printf "========= type generator ==========\n";
-    printf "%s\n" (Sexp.to_string_hum (BaseType.sexp_of_t values));
-    printf "========= staged generator ==========\n";
-    printf "%s\n" (Sexp.to_string_hum (BaseType_Staged_SR.sexp_of_t staged_values_sr));
-    printf "========= staged generator c ==========\n";
-    printf "%s\n" (Sexp.to_string_hum (BaseType_Staged_C.sexp_of_t staged_values_sr));
-    printf "========= staged generator csr ==========\n";
-    printf "%s\n" (Sexp.to_string_hum (BaseType_Staged_CSR.sexp_of_t staged_values_sr))
-  done
-*)
+    let staged_values = Base_quickcheck.Generator.generate generator ~size ~random:random_b in
+    printf "========= Staged generator ==========\n";
+    printf "%s\n" (Sexp.to_string_hum (sexp_of_nat staged_values))
+  done *)
+
 (*
   dune exec BST -- qcheck prop_InsertInsert bespoke out
   dune exec BST -- qcheck prop_InsertInsert type out
@@ -66,7 +58,7 @@ let () =
 
 let properties : (string * tree property) list =
   [
-    ("prop_InsertValid", test_prop_InsertValid);
+    (* ("prop_InsertValid", test_prop_InsertValid);
     ("prop_DeleteValid", test_prop_DeleteValid);
     ("prop_UnionValid", test_prop_UnionValid);
     ("prop_InsertPost", test_prop_InsertPost);
@@ -78,12 +70,12 @@ let properties : (string * tree property) list =
     ("prop_InsertInsert", test_prop_InsertInsert);
     ("prop_InsertDelete", test_prop_InsertDelete);
     ("prop_InsertUnion", test_prop_InsertUnion);
-    ("prop_DeleteInsert", test_prop_DeleteInsert);
+    ("prop_DeleteInsert", test_prop_DeleteInsert); *)
     ("prop_DeleteDelete", test_prop_DeleteDelete);
-    ("prop_DeleteUnion", test_prop_DeleteUnion);
+    (* ("prop_DeleteUnion", test_prop_DeleteUnion);
     ("prop_UnionDeleteInsert", test_prop_UnionDeleteInsert);
     ("prop_UnionUnionIdem", test_prop_UnionUnionIdem);
-    ("prop_UnionUnionAssoc", test_prop_UnionUnionAssoc);
+    ("prop_UnionUnionAssoc", test_prop_UnionUnionAssoc); *)
   ]
 
 let qstrategies : (string * tree arbitrary) list =
@@ -93,6 +85,12 @@ let cstrategies : (string * tree gen) list =
   []
 
 let bstrategies : (string * tree basegen) list =
-  [("bespoke", (module BaseBespoke)); ("type", (module BaseType)); ("staged", (module BaseType_Staged_C)); ("stagedC", (module BaseType_Staged_C)); ("stagedCSR", (module BaseType_Staged_CSR))]
+  [
+    ("bespoke", (module BaseBespoke));
+    ("type", (module BaseType));
+    ("staged", (module BaseType_Staged_SR));
+    ("stagedC", (module BaseType_Staged_C));
+    ("stagedCSR", (module BaseType_Staged_CSR))
+  ]
 
 let () = main properties qstrategies cstrategies bstrategies
